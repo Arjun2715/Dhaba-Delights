51bc6ea0220a3f8bdf73eaf86c1281931eb9371e
Arjun <arjunjot18@gmail.com>
2024-03-23 12:04:22 +0100

    fix

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,22 +4,22 @@ export default function header() {
     return <div className=" top-0 z-50 ">
         <div className="flex flex-row justify-between h-20 w-full px-6  sm:px-4 md:px-2 ">
             <div className="self-end">
-                <Link href="/" className=" font-medium sm:font-bold md:text-3xl ">Dhaba Delights</Link>
+                <Link href="/" className=" font-bold md:text-3xl sm:text-2xl text-xl ">Dhaba Delights</Link>
             </div>
             <div className="self-end">
                 <nav>
                     <ul className="flex flex-row space-x-4">
                         <li className="cursor-pointer ">
-                            <Link className="md:font-medium md:text-xl hover:text-orange-500 " href="/">Home</Link>
+                            <Link className="font-medium md:text-xl hover:text-orange-500 " href="/">Home</Link>
                         </li>
                         <li className="cursor-pointer hover:underlin">
-                            <Link className="md:font-medium md:text-xl hover:text-orange-500 " href="/shop">Shop</Link>
+                            <Link className="font-medium md:text-xl hover:text-orange-500 " href="/shop">Shop</Link>
                         </li>
                         {/* <li className="cursor-pointer">
                             <Link href="/about">About us</Link>
                         </li> */}
                         <li className="cursor-pointer hover:underlin">
-                            <Link className="md:font-medium md:text-xl hover:text-orange-500 " href="/contact">Contact</Link>
+                            <Link className=" md:text-xl hover:text-orange-500 " href="/contact">Contact</Link>
                         </li>
                     </ul>
                 </nav>
