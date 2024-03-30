70a2a4bdbc5e37ed358585cc79b4fe27f144736f
Arjun <arjunjot18@gmail.com>
2024-03-31 00:11:08 +0100

    speedinsight

--- app/components/locationMap.tsx
+++ app/components/locationMap.tsx
@@ -7,7 +7,7 @@ export default function AddressMap(){
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d299399.11135053955!2d-0.021668943998047873!3d51.60227391373084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8b1a0b7414329%3A0x1990908601e30626!2s8%20Hardy%20Grove%2C%20Dartford%20DA1%205NU%2C%20UK!5e0!3m2!1sen!2ses!4v1710018194811!5m2!1sen!2ses"
                 // width="600"
                 // height="450" 
-                className='w-full h-[500px] rounded-lg m-6 mx-[20px]'
+                className='w-full h-[500px] rounded-lg'
                 style={{ border: 0 }} 
                 aria-hidden="false" 
             />
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,58 +2,52 @@ import Map from "@/app/components/locationMap"
 import Link from 'next/link';
 
 export default function contact() {
-    return <main className="flex flex-col  text-slate-900 overflow-x-hidden">
+    return <main className="flex flex-col  text-slate-900 mx-10">
         <div className="my-8">
             <h1 className="text-4xl font-medium ">
                 Contact Us
             </h1>
         </div>
-        <div className="flex flex-col md:w-full justify-center mb-14">
-            <div className="flex md:flex-nowrap sm:flex-wrap  md:flex-row xs:flex-col justify-evenly m-8 ">
-                {/* phone */}
-                <div className="flex justify-center w-full h-[200px] m-4 rounded-xl  shadow-md hover:shadow-xl">
+        <div className="flex flex-col w-full justify-center mb-14">
+            <div className="flex flex-col md:flex-row md:flex-nowrap flex-wrap justify-evenly md:space-x-6">
+
+                <div className="flex justify-center w-full md:w-1/4 h-48 md:h-[200px] mb-4 rounded-xl shadow-md hover:shadow-xl">
                     <div className="px-6 py-4 self-center">
                         <div className="font-bold text-2xl mb-2 flex justify-center">Phone number</div>
                         <p className="text-gray-700 text-xl flex justify-center">
-                            <div className="flex flex-col">
-                                <Link href="tel:00447466891903" className="self-center">English: +44 7466891903</Link>
-                                <Link href="tel:00447460411957" className="self-center">Punjabi: +44 7460411957</Link>
-
+                            <div className="flex flex-col items-center justify-center">
+                                <a className="flex flex-col md:flex-row justify-center" href="tel:00447466891903"> <p className="self-center">English:</p>  <p className="whitespace-nowrap">+44 7466891903</p></a>
+                                <a className="flex flex-col md:flex-row justify-center" href="tel:00447460411957"> <p className="self-center">Punjabi:</p>  <p className="whitespace-nowrap">+44 7460411957</p></a>
                             </div>
                         </p>
                     </div>
                 </div>
-                {/* Insta */}
-                <div className="flex justify-center w-full h-[200px]  m-4 rounded-xl overflow-hidden shadow-md hover:shadow-xl">
+                <div className="flex justify-center w-full md:w-1/4 h-48 md:h-[200px] mb-4 rounded-xl overflow-hidden shadow-md hover:shadow-xl">
                     <div className="px-6 py-4 self-center">
                         <div className="font-bold text-2xl mb-2 flex justify-center">Instagram</div>
                         <p className="text-gray-700 text-xl flex justify-center">
-                            <Link href="#">@instagram</Link>
+                            <a href="#">@instagram</a>
                         </p>
                     </div>
                 </div>
-                {/* Email */}
-                <div className="flex justify-center w-full h-[200px]  m-4 rounded-xl overflow-hidden shadow-md hover:shadow-xl">
+                <div className="flex justify-center w-full md:w-1/4 h-48 md:h-[200px] mb-4 rounded-xl overflow-hidden shadow-md hover:shadow-xl">
                     <div className="px-6 py-4 self-center">
                         <div className="font-bold text-2xl mb-2 flex justify-center">Email</div>
                         <p className="text-gray-700 text-xl flex justify-center">
-                            <Link href="mailto:as1211300@gmail.com">as1211300@gmail.com</Link>
+                            <a href="mailto:as1211300@gmail.com">as1211300@gmail.com</a>
                         </p>
                     </div>
                 </div>
-                {/* Tiktok */}
-                <div className="flex justify-center w-full h-[200px]  m-4 rounded-xl overflow-hidden shadow-md hover:shadow-xl">
+                <div className="flex justify-center w-full md:w-1/4 h-48 md:h-[200px] mb-4 rounded-xl overflow-hidden shadow-md hover:shadow-xl">
                     <div className="px-6 py-4 self-center">
                         <div className="font-bold text-2xl mb-2 flex justify-center">Tiktok</div>
-                        <p className="text-gray-700 text-xl flex justify-center">
-                            <Link href="#">@dhaba-delightes</Link>
+                        <p className="text-gray-700 text-xl flex justify-center text-nowrap">
+                            <a href="#">@dhaba-delightes</a>
                         </p>
                     </div>
                 </div>
-            </div>
-
-            <Map />
-
+            </div> 
+            <Map /> 
         </div>
     </main>
 }
